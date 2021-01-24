dfeae1fa30074a2b84273459d39a45934e7fed09
vazgen4073 <4073mkrtchyan@mail.ru>
2021-01-24 16:05:00 +0400

    added file

---
+++ rotate_image_without_STL.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+int** rotateImage(int** a, int n)
+{
+    if (n <= 1) {
+        return a;
+    }
+
+    int counter = 0, draw_counter = 0, temp_1, temp_2;
+    temp_1 = a[0][0];
+    int i = 0, j = 0, size = n, local_size, lc_counter = 0;
+    local_size = size - lc_counter;
+
+    while (lc_counter < size / 2) {
+
+        if (counter == 0) {
+            while (j < size - lc_counter) {
+                temp_2 = a[i][j];
+                a[i][j] = temp_1;
+                temp_1 = temp_2;
+                j++;
+            }
+            j--;
+            i++;
+        }
+        if (counter == 1) {
+            while (i < size - lc_counter) {
+                temp_2 = a[i][j];
+                a[i][j] = temp_1;
+                temp_1 = temp_2;
+                i++;
+            }
+            i--;
+            j--;
+        }
+        if (counter == 2) {
+            while (j >= lc_counter) {
+                temp_2 = a[i][j];
+                a[i][j] = temp_1;
+                temp_1 = temp_2;
+                j--;
+            }
+            j++;
+            i--;
+        }
+        if (counter == 3) {
+            while (i >= lc_counter) {
+                temp_2 = a[i][j];
+                a[i][j] = temp_1;
+                temp_1 = temp_2;
+                i--;
+            };
+            i++;
+
+            local_size = size - lc_counter;
+            counter = -1;
+            draw_counter++;
+
+            if (draw_counter == local_size - lc_counter - 1) {
+                draw_counter = 0;
+                lc_counter++;
+                i = lc_counter;
+                j = lc_counter;
+            }
+        }
+
+        counter++;
+    }
+
+    return a;
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    int** vec = new int*[n];
+    for (int i = 0; i < n; i++) {
+        vec[i] = new int[n];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cin >> vec[i][j];
+        }
+    }
+
+    rotateImage(vec, n);
+
+    for (int i = 0; i < n; i++) {
+        delete[] vec[i];
+    }
+    delete[] vec;
+}
+
